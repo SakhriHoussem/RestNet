bb5ccd7ec83d1fe1d2ea6b5ba615cdf50de79007
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-02 20:41:29 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -10,14 +10,16 @@ def datSetGenerator(path):
     classes = os.listdir(path)
     print(len(classes))
     image_list = []
+    labels = []
     for classe in classes:
         for filename in glob.glob(path+'/'+classes[1]+'/*.tif'): #assuming gif
             im=Image.open(filename)
             label=np.zeros(len(classes),dtype=int)
             label[classes.index(classe)]=1
-            image_list.append([img_to_tensor(im),label])
-    return  image_list
+            image_list.append([img_to_tensor(im)])
+            labels.append(label)
+    return  image_list,labels
 
 path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-image_list = datSetGenerator(path)
-print(len(image_list),len(image_list[0]))
+image_list,labels = datSetGenerator(path)
+print(image_list[756],labels[756])
