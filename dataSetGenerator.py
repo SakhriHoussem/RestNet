2db8405c77c469b7bb441c883f6cc629ae02f483
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-02 23:21:58 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -6,20 +6,23 @@ import glob
 
 def img_to_tensor(img):
     return tf.convert_to_tensor(np.asarray(img,np.float32),np.float32)
+
 def datSetGenerator(path):
     classes = os.listdir(path)
-    print(len(classes))
     image_list = []
     labels = []
     for classe in classes:
-        for filename in glob.glob(path+'/'+classes[1]+'/*.tif'): #assuming gif
+        for filename in glob.glob(path+'/'+classe+'/*.tif'): #assuming gif
             im=Image.open(filename)
             label=np.zeros(len(classes),dtype=int)
             label[classes.index(classe)]=1
             image_list.append([img_to_tensor(im)])
             labels.append(label)
-    return  image_list,labels
-
+    return  image_list,labels,classes
+"""
 path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-image_list,labels = datSetGenerator(path)
-print(image_list[756],labels[756])
+image_list,labels,classes = datSetGenerator(path)
+print(classes)
+print(image_list[699])
+print(labels[699])
+"""
