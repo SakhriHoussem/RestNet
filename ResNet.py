fa9086bc1b9b080c271624b9a3b19cba96f31b5a
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-03 15:36:46 +0100

    Add files via upload

--- ResNet.py
+++ ResNet.py
@@ -26,7 +26,7 @@ def batchNorm_layer(): print("---BatchNorm---")
 
 def scale_layer(): print("-----Scale-----")
 
-def relu_layer(x):return tf.nn.relu(x)
+#def relu_layer(x):return tf.nn.relu(x)
 
 
 def conv_layer(data,num_filters,filter_size,strides=1,padding="SAME",use_bias=True,use_relu=True):
@@ -54,7 +54,7 @@ def conv_layer(data,num_filters,filter_size,strides=1,padding="SAME",use_bias=Tr
 
     scale_layer()
 
-    if use_relu: conv = relu_layer(conv)
+    if use_relu: conv = tf.nn.relu(conv)
 
     return conv
 
@@ -71,62 +71,81 @@ def stepConvConvConv(data,num_filters=128,filter_size=1,strides=1):
     return conv23
 
 def block1(data,num_filters=64):
+    print("BLOCK1 #########################")
     conv23 = stepConvConv(data,num_filters)
     convi = conv_layer(data,num_filters*4,1,1,padding="VALID",use_bias=False,use_relu=False)
-    return convi+conv23
+    return tf.add(conv23, convi)
 
 def blockend(data,num_filters=64):
     conv21=conv_layer(data,num_filters*4,1,2,padding="VALID",use_bias=False,use_relu=False)
     convv = stepConvConvConv(data,num_filters,filter_size=1,strides=1)
-    return relu_layer(conv21+convv)
+    return tf.nn.relu(tf.add(conv21, convv))
 
 
 def block2(data,num_filters=64):
+    print("BLOCK2 #########################")
 
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     data = blockend(data,num_filters*2)
     return data
 
 
 
 def block3(data,num_filters=128):
+    print("BLOCK3 #########################")
 
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     data = blockend(data,num_filters*2)
     return data
 def block4(data,num_filters=256):
+    print("BLOCK4 #########################")
 
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     data = blockend(data,num_filters*2)
     return data
 def block5(data,num_filters=512):
+    print("BLOCK5 #########################")
 
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
     conv23 = stepConvConv(data,num_filters)
-    data= relu_layer(data+conv23)
+    data= tf.nn.relu(tf.add(conv23, data))
 
     return pool_avg(data,7,1,'VALID')
 
-def my_model(data,num_filters=64,filter_size=7,padding='SAME',strides=2):
+def fc_layer(data,num_filters=21):
+    print("FC LAYER #########################")
 
+    n,height,width,channels=tf.shape(data).eval()
+
+    data = tf.reshape(data,[-1,height*width*channels])
+    # generate weigh [kernal size,kernal size,channel,number of filters]
+    w=weight_generater([height*width*channels,num_filters])
+
+    # for each filter W has his  specific bias
+    b=bias_generater(num_filters)
+
+    matmul_fc = tf.matmul(data,w)+b
+
+    return tf.nn.softmax(tf.nn.relu(matmul_fc))
+def my_model(data,num_filters=64,filter_size=7,padding='SAME',strides=2):
     conv1=conv_layer(data,num_filters,filter_size,strides)
     pool1=pool_max_2x2(conv1)
     b1 = block1(pool1)
@@ -134,10 +153,7 @@ def my_model(data,num_filters=64,filter_size=7,padding='SAME',strides=2):
     b3 = block3(b2)
     b4 = block4(b3)
     b5 = block5(b4)
-
-    return b5
-
-
+    return fc_layer(b5)
 
 sess=tf.InteractiveSession()
 # load image
@@ -165,4 +181,6 @@ sess.run(tf.global_variables_initializer())
 
 result=sess.run(resnet50)
 print(tf.shape(result).eval())
-sess.close()
+print(result)
+print("index",np.argmax(result))
+print(classes[np.argmax(result)])
