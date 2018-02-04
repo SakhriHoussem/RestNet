6de313893cadd23ff3eefa40dfeedb1f128e02bd
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-04 19:55:17 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -1,25 +1,27 @@
-import tensorflow as tf
 import numpy as np
 import os
-from PIL import Image
+import cv2
 import glob
+import time
 
 def datSetGenerator(path):
+    start_time = time.time()
     classes = os.listdir(path)
     image_list = []
     labels = []
     for classe in classes:
-        for filename in glob.glob(path+'/'+classe+'/*.tif'): #assuming gif
-            image_list.append(np.asarray(Image.open(filename),np.float32))
-            label=np.zeros(len(classes),dtype=int)
-            label[classes.index(classe)]=1.
+        for filename in glob.glob(path+'/'+classe+'/*.tif'):
+            image_list.append(cv2.imread(filename))
+            label=np.zeros(len(classes))
+            label[classes.index(classe)]=1
             labels.append(label)
-    return  image_list,labels,classes
-
+    print("\n --- dataSet generated in  %s seconds --- \n" % (time.time() - start_time))
+    return  np.array(image_list),np.array(labels),np.array(classes)
 """
+# for testing the generator
 path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-data,labels,classes = datSetGenerator(path)
-print(classes)
-print(data[699])
-print(labels[699])
+test,tlabels,classes = datSetGenerator(path)
+print("\n dataSet classes :\n",classes.shape)
+print("\n data shape :",test.shape)
+print("\n labels shape :",tlabels.shape)
 """
