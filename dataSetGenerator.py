e31c850a38be745ecfd712ee1ad6065a28e7a2ac
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-06 19:59:03 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -1,9 +1,12 @@
 import numpy as np
 import os
-import cv2
 import glob
 import time
+import cv2
+import tensorflow as tf
 
+def img_to_tensor(img):
+    return tf.convert_to_tensor(img,np.float32)
 def datSetGenerator(path):
     start_time = time.time()
     classes = os.listdir(path)
@@ -11,17 +14,20 @@ def datSetGenerator(path):
     labels = []
     for classe in classes:
         for filename in glob.glob(path+'/'+classe+'/*.tif'):
-            image_list.append(cv2.imread(filename))
+            image_list.append(cv2.resize(cv2.imread(filename),(224, 224)))
             label=np.zeros(len(classes))
             label[classes.index(classe)]=1
             labels.append(label)
     print("\n --- dataSet generated in  %s seconds --- \n" % (time.time() - start_time))
-    return  np.array(image_list),np.array(labels),np.array(classes)
-"""
-# for testing the generator
-path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-test,tlabels,classes = datSetGenerator(path)
-print("\n dataSet classes :\n",classes.shape)
-print("\n data shape :",test.shape)
-print("\n labels shape :",tlabels.shape)
-"""
+    return np.array(image_list),np.array(labels),np.array(classes)
+
+
+if __name__ == '__main__':
+
+    # for testing the generator
+    path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
+    data,labels,classes = datSetGenerator(path)
+    print("\n dataSet classes :",classes.shape)
+    print("\n data shape :",data.shape)
+    print("\n labels shape :",labels.shape)
+
