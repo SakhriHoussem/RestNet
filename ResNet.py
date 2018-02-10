44831014dbb4ff6f6bf4e76223c5efb8e7b6f20b
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-10 22:15:58 +0100

    Add files via upload

--- ResNet.py
+++ ResNet.py
@@ -14,20 +14,33 @@ def weight_generater(shape):
     return tf.Variable(tf.truncated_normal(shape,stddev=0.05),name="Weight")
 
 def bias_generater(shape):
-    return tf.Variable(tf.zeros([shape]),name='bias')
+    return tf.Variable(tf.zeros([shape]),name="Weight")
 
 def conv2d(x,W,strides=1,padding='SAME'):
     return tf.nn.conv2d(x,W,[1,strides,strides,1],padding='SAME')
 
-def pool_max_2x2(x,ksize=2,strides=2,padding='SAME'):
+def pool_max(x,ksize=2,strides=2,padding='SAME'):
     return tf.nn.max_pool(x,ksize=[1,ksize,ksize,1],strides=[1,strides,strides,1],padding=padding)
 
 def pool_avg(x,ksize=2,strides=2,padding='SAME'):
     return tf.nn.avg_pool(x,ksize=[1,ksize,ksize,1],strides=[1,strides,strides,1],padding=padding)
 
-def batchNorm_layer(): print("---BatchNorm---")
+def batchNorm_layer(inputs, is_training, decay = 0.999,epsilon = 1e-3):
 
-def scale_layer(): print("-----Scale-----")
+    scale = tf.Variable(tf.ones(inputs.get_shape()[1:].as_list()))
+    beta = tf.Variable(tf.zeros(inputs.get_shape()[1:].as_list()))
+    pop_mean = tf.Variable(tf.zeros(inputs.get_shape()[1:].as_list()), trainable=False)
+    pop_var = tf.Variable(tf.ones(inputs.get_shape()[1:].as_list()), trainable=False)
+
+    if is_training:
+        batch_mean, batch_var = tf.nn.moments(inputs,[0])
+        train_mean = tf.assign(pop_mean,pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var,
+                              pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs,batch_mean, batch_var, beta, scale, epsilon)
+    else:
+        return tf.nn.batch_normalization(inputs,pop_mean, pop_var, beta, scale, epsilon)
 
 def conv_layer(data,num_filters,filter_size,strides=1,padding="SAME",use_bias=True,use_relu=True):
     # input shape [W,H,channels]
@@ -50,8 +63,7 @@ def conv_layer(data,num_filters,filter_size,strides=1,padding="SAME",use_bias=Tr
         global bias
         bias +=1
         conv=tf.add(conv,b)
-    batchNorm_layer()
-    scale_layer()
+    conv = tf.add(batchNorm_layer(conv,True),b)
     if use_relu: return tf.nn.relu(conv)
     return conv
 
@@ -190,6 +202,86 @@ def stepConvConvConv(data,num_filters=128,filter_size=1,strides=1):
     conv_2=conv_layer(conv_1,num_filters,3,strides,padding='SAME',use_bias=False)
     return conv_layer(conv_2,num_filters*4,filter_size,strides,padding="VALID",use_bias=False,use_relu=False)
 
+def blockend(data,num_filters=64):
+
+    """
+    :param data:
+    :param num_filters:
+    :return:
+                          input
+                            |
+               ----------------------------
+              |                           |
+             \/                          \/
+        +------------+              +------------+
+          conv                       conv
+          num_filters 128            num_filters 128*4
+          filter_size 1              filter_size 1
+          padding 0                  padding 0
+          stride 2                   stride 1
+          False                      False
+        +------------+              +------------+
+          BatchNorm                  BatchNorm
+          True                       True
+        +------------+              +------------+
+          Scale                      Scale
+          True                       True
+        +------------+              +------------+
+          relu                       relu
+        +------------+              +------------+
+              |                           |
+             \/                           |
+        +------------+                    |
+          conv                            |
+          num_filters 128                 |
+          filter_size 3                   |
+          padding 1                       |
+          stride 1                        |
+          False                           |
+        +------------+                    |
+          BatchNorm                       |
+          True                            |
+        +------------+                    |
+          Scale                           |
+          True                            |
+        +------------+                    |
+          relu                            |
+        +------------+                    |
+              |                           |
+             \/                           |
+          +------------+                  |
+          conv                            |
+          num_filters 128*4               |
+          filter_size 1                   |
+          padding 0                       |
+          stride 1                        |
+          False                           |
+        +------------+                    |
+          BatchNorm                       |
+          True                            |
+        +------------+                    |
+          Scale                           |
+          True                            |
+        +------------+                    |
+          relu                            |
+        +------------+                    |
+              |                           |
+              ----------------------------
+                           |
+                          \/
+                    +------------+
+                         res
+                    +------------+
+                        relu
+                    +------------+
+                          |
+                         \/
+    """
+    conv_1=conv_layer(data,num_filters*4,1,2,padding="VALID",use_bias=False,use_relu=False)
+    conv_2=stepConvConvConv(data,num_filters,filter_size=1,strides=1)
+    return tf.nn.relu(tf.add(conv_1, conv_2))
+
+
 def block1(data,num_filters=64):
     """
 
@@ -270,86 +362,6 @@ def block1(data,num_filters=64):
     conv_2=conv_layer(data,num_filters*4,1,1,padding="VALID",use_bias=False,use_relu=False)
     return tf.add(conv_1,conv_2)
 
-def blockend(data,num_filters=64):
-
-    """
-
-    :param data:
-    :param num_filters:
-    :return:
-                          input
-                            |
-               ----------------------------
-              |                           |
-             \/                          \/
-        +------------+              +------------+
-          conv                       conv
-          num_filters 128            num_filters 128*4
-          filter_size 1              filter_size 1
-          padding 0                  padding 0
-          stride 2                   stride 1
-          False                      False
-        +------------+              +------------+
-          BatchNorm                  BatchNorm
-          True                       True
-        +------------+              +------------+
-          Scale                      Scale
-          True                       True
-        +------------+              +------------+
-          relu                       relu
-        +------------+              +------------+
-              |                           |
-             \/                           |
-        +------------+                    |
-          conv                            |
-          num_filters 128                 |
-          filter_size 3                   |
-          padding 1                       |
-          stride 1                        |
-          False                           |
-        +------------+                    |
-          BatchNorm                       |
-          True                            |
-        +------------+                    |
-          Scale                           |
-          True                            |
-        +------------+                    |
-          relu                            |
-        +------------+                    |
-              |                           |
-             \/                           |
-          +------------+                  |
-          conv                            |
-          num_filters 128*4               |
-          filter_size 1                   |
-          padding 0                       |
-          stride 1                        |
-          False                           |
-        +------------+                    |
-          BatchNorm                       |
-          True                            |
-        +------------+                    |
-          Scale                           |
-          True                            |
-        +------------+                    |
-          relu                            |
-        +------------+                    |
-              |                           |
-              ----------------------------
-                           |
-                          \/
-                    +------------+
-                         res
-                    +------------+
-                        relu
-                    +------------+
-                          |
-                         \/
-    """
-    conv_1=conv_layer(data,num_filters*4,1,2,padding="VALID",use_bias=False,use_relu=False)
-    conv_2=stepConvConvConv(data,num_filters,filter_size=1,strides=1)
-    return tf.nn.relu(tf.add(conv_1, conv_2))
-
 def block2(data,num_filters=64):
     print("BLOCK2 -----------------------------------")
     conv=stepConvConv(data,num_filters)
@@ -392,71 +404,74 @@ def fc_layer(data,num_classes=21):
     print("FC LAYER -----------------------------------")
     n,height,width,channels=data.get_shape().as_list()
     print("---fc size---\n",height,width,channels)
-
     data=tf.reshape(data,[-1,height*width*channels])
     # generate weigh [kernal size,kernal size,channel,number of filters]
     w=weight_generater([height*width*channels,num_classes])
     # for each filter W has his  specific bias
     b=bias_generater(num_classes)
     return tf.add(tf.matmul(data,w),b)
-    #return tf.nn.softmax(tf.nn.relu(matmul_fc))
 
 def my_model(data,num_classes=21,num_filters=64,filter_size=7,padding='SAME',strides=2):
     conv_1=conv_layer(data,num_filters,filter_size,strides)
-    pool_1=pool_max_2x2(conv_1,ksize=3)
+    pool_1=pool_max(conv_1,ksize=3)
     b_1=block1(pool_1)
     b_2=block2(b_1)
     b_3=block3(b_2)
     b_4=block4(b_3)
     b_5=block5(b_4)
-    return tf.nn.relu(fc_layer(b_5,num_classes))
-
-# Generate datSets
-path = "C:/Users/shous/Desktop/UCMerced_LandUse - train/Images/"
-
-#path="C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-data,labels,classes=datSetGenerator(path)
+    return fc_layer(b_5,num_classes)
 
 
-# get image height,width,channels
-n,height,width,channels=data.shape
-print("--Input image--\n",height,width,channels)
+
+if __name__ == '__main__':
+
+    # Generate datSets
+    path = "C:/Users/shous/Desktop/UCMerced_LandUse - train/Images/"
+
+    #path="C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
+    data,labels,classes=datSetGenerator(path)
 
 
-# number of classes
-num_classes=len(classes)
+    # get image height,width,channels
+    n,height,width,channels=data.shape
+    print("--Input image--\n",height,width,channels)
 
-x=tf.placeholder(tf.float32,[None,height,width,channels])
-y=tf.placeholder(tf.float32,[None,num_classes])
 
-#sess = tf.InteractiveSession()
-logits=my_model(x,num_classes)
-softmax=tf.nn.softmax(logits)
+    # number of classes
+    num_classes=len(classes)
 
-cost=tf.reduce_mean(tf.abs(y - softmax))
-train=tf.train.GradientDescentOptimizer(0.05).minimize(cost)
+    x=tf.placeholder(tf.float32,[None,height,width,channels])
+    y=tf.placeholder(tf.float32,[None,num_classes])
 
-correct_prediction=tf.equal(tf.argmax(softmax),tf.argmax(y))
-acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    #sess = tf.InteractiveSession()
+    logits=my_model(x,num_classes)
 
-print("we have ",weights," weights",bias," bias  ------------")
+    # Define a loss function
+    #loss=tf.reduce_mean(tf.abs(y- logits))
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2 (labels=y, logits=logits)
 
-batch_size = 10
-iteration =10
+    #loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(logits), reduction_indices=1))
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    file=tf.summary.FileWriter("graph/",sess.graph)
-    for _ in range(iteration):
-        indice = np.random.permutation(n)
-        for i in range(n):
-            min_batch = indice[i*batch_size:(i+1)*batch_size]
-            print(_,"-- Train : ",sess.run(train,feed_dict={x:data[min_batch],y:labels[min_batch]}))
+    train = tf.train.GradientDescentOptimizer(learning_rate=0.05).minimize(loss)
 
-    #print("\n\nLogits of model : \n\n",sess.run(logits,feed_dict={x:data[0],y:labels[0]}))
-    #print("Softmax of model : \n\n",sess.run(softmax,feed_dict={x:data[0],y:labels[0]}))
-    print("Cost : ",sess.run(cost,feed_dict={x:data,y:labels}))
-    print("Accuracy  : ",sess.run(acc,feed_dict={x:data,y:labels}))
+    #correct_prediction=tf.equal(tf.argmax(softmax),tf.argmax(y))
+    #acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
-print("--- %s seconds ---" % (np.round(time.time() - start_time)))
-sess.close()
+    print("we have ",weights," weights",bias," bias  ------------")
+
+    batch_size = 5
+    iteration = 4
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        file=tf.summary.FileWriter("graph/",sess.graph)
+        for _ in range(iteration):
+            s = sess.run(loss,feed_dict={x:data,y:labels})
+            print("loss : ",s.shape,s)
+            indice = np.random.permutation(n)
+            for i in range(n):
+                min_batch = indice[i*batch_size:(i+1)*batch_size]
+                curr_loss,_ = sess.run([loss, train], {x:data[min_batch], y:labels[min_batch]})
+                print("Iteration %d \n  loss: \n %s " % (i, curr_loss))
+
+    print("--- %s seconds ---" % (np.round(time.time() - start_time)))
