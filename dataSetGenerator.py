54e6f7e4b48d51f97d13890a1afda26c29f2b56f
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-12 20:16:02 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -3,8 +3,9 @@ from os import listdir
 from glob import glob
 from time import time
 import cv2
+import matplotlib.pyplot as plt
 
-def dataSetGenerator(path,resize=False,resize_to=224):
+def dataSetGenerator(path,resize=False,resize_to=224,percentage=100):
     start_time = time()
     classes = listdir(path)
     image_list = []
@@ -16,13 +17,17 @@ def dataSetGenerator(path,resize=False,resize_to=224):
             label=np.zeros(len(classes))
             label[classes.index(classe)]=1
             labels.append(label)
+    indice = np.random.permutation(len(image_list))[:int(len(image_list)*percentage/100)]
     print("\n --- dataSet generated in  %s seconds --- \n" % (np.round(time() - start_time)))
-    return np.array(image_list,float),np.array(labels,float),np.array(classes)
+    return np.array([image_list[x] for x in indice]),np.array([labels[x] for x in indice]),np.array(classes)
 
 if __name__ == '__main__':
     # for testing the generator
     path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-    data,labels,classes = dataSetGenerator(path,True)
+    data,labels,classes = dataSetGenerator(path,percentage=20)
     print("\n dataSet classes :\n",*classes)
-    print("\n data shape :",data.shape)
-    print("\n label shape :",labels.shape)
+    print("\n   data shape  : ",data.shape)
+    print("\n label shape :",labels[100])
+    plt.imshow(data[100])
+    plt.show()
+
