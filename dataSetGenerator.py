7f2f33893c126b7fe447f8762a505ac1d5dd6526
Sakhri Houssem <SakhriHoussem@users.noreply.github.com>
2018-02-15 23:00:39 +0100

    Add files via upload

--- dataSetGenerator.py
+++ dataSetGenerator.py
@@ -1,33 +1,1 @@
-import numpy as np
-from os import listdir
-from glob import glob
-from time import time
-import cv2
-import matplotlib.pyplot as plt
-
-def dataSetGenerator(path,resize=False,resize_to=224,percentage=100):
-    start_time = time()
-    classes = listdir(path)
-    image_list = []
-    labels = []
-    for classe in classes:
-        for filename in glob(path+'/'+classe+'/*.tif'):
-            if resize: image_list.append(cv2.resize(cv2.imread(filename),(resize_to, resize_to)))
-            else: image_list.append(cv2.imread(filename))
-            label=np.zeros(len(classes))
-            label[classes.index(classe)]=1
-            labels.append(label)
-    indice = np.random.permutation(len(image_list))[:int(len(image_list)*percentage/100)]
-    print("\n --- dataSet generated in  %s seconds --- \n" % (np.round(time() - start_time)))
-    return np.array([image_list[x] for x in indice]),np.array([labels[x] for x in indice]),np.array(classes)
-
-if __name__ == '__main__':
-    # for testing the generator
-    path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
-    data,labels,classes = dataSetGenerator(path,percentage=20)
-    print("\n dataSet classes :\n",*classes)
-    print("\n   data shape  : ",data.shape)
-    print("\n label shape :",labels[100])
-    plt.imshow(data[100])
-    plt.show()
-
+import numpy as npfrom os import listdirfrom glob import globfrom time import timeimport cv2import matplotlib.pyplot as pltdef dataSetGenerator(path,resize=False,resize_to=224,percentage=100):    """    DataSetsFolder      |      |----------class-1      |        .   |-------image-1      |        .   |         .      |        .   |         .      |        .   |         .      |        .   |-------image-n      |        .      |-------class-n    :param path: <path>/DataSetsFolder    :param resize:    :param resize_to:    :param percentage:    :return: images, labels, classes    """    start_time = time()    classes = listdir(path)    image_list = []    labels = []    for classe in classes:        for filename in glob(path+'/'+classe+'/*.tif'):            if resize:image_list.append(cv2.resize(cv2.imread(filename),(resize_to, resize_to)))            else:image_list.append(cv2.imread(filename))            label=np.zeros(len(classes))            label[classes.index(classe)]=1            labels.append(label)    indice = np.random.permutation(len(image_list))[:int(len(image_list)*percentage/100)]    print("\n --- dataSet generated in  %s seconds --- \n" % (np.round(time()-start_time)))    return np.array([image_list[x] for x in indice]),np.array([labels[x] for x in indice]),np.array(classes)if __name__ == '__main__':    # for testing the generator    path = "C:/Users/shous/Desktop/UCMerced_LandUse/Images/"    data,labels,classes = dataSetGenerator(path,percentage=80)    print("\n dataSet classes :\n",*classes)    print("\n   data shape  : ",data.shape)    print("\n label shape :",labels[100])    plt.imshow(data[100])    plt.show()
--- train.py
+++ train.py
@@ -2,41 +2,90 @@ import tensorflow as tf
 from dataSetGenerator import dataSetGenerator
 import numpy as np
 import matplotlib.pyplot as plt
+from os import path
+from ResNet import ResNet50
 
+
+# Generate datSets
+# path="C:/Users/shous/Desktop/UCMerced_LandUse/Images/"
 data, labels, classes = dataSetGenerator("C:/Users/shous/Desktop/UCMerced_LandUse - train/Images/")
-tf.reset_default_graph()
-saver = tf.train.import_meta_graph("Save/dataSaved.meta")
 batch_size = 10
-epochs = 1
-#errors = []
-batche_num=data.shape[0]
-with tf.Session() as sess:
-    saver.restore(sess,tf.train.latest_checkpoint('Save/'))
+epochs = 10
+save_dir = "Save/"
+save_file ="dataSaved"
 
-    graph = tf.get_default_graph()
-    #[print(n.name) for n in graph.as_graph_def().node]
-    x = graph.get_tensor_by_name("t_picture:0")#vrai
-    y = graph.get_tensor_by_name("t_labels:0")#vrai
-    #train = graph.get_operation_by_name("Train_op")
-    #logists = graph.get_operation_by_name("logits")
-    #loss = graph.get_operation_by_name("Loss")
-    train = tf.get_collection('train_op')#vrai
-    loss = tf.get_collection('loss_op')#vrai
-    logists = tf.get_collection('logits_op')#vrai
-    errors = tf.get_collection('errors')#vrai
-    print(errors)
-    saver = tf.train.Saver()
-    #sess.run(tf.global_variables_initializer())
+#tf.reset_default_graph()
+
+
+# get image height, width, channels
+batche_num, height, width, channels = data.shape
+print("Input image size :", height, width, channels)
+with tf.Session() as sess:
+
+    if path.isdir(save_dir) and path.isfile(save_dir+save_file+".meta") and path.isfile(save_dir+"checkpoint"):
+        print("file exist")
+        saver = tf.train.import_meta_graph(save_dir+save_file+".meta")
+        saver.restore(sess, tf.train.latest_checkpoint('Save/'))
+        print("data restored")
+        graph = tf.get_default_graph()
+        #[print(n.name) for n in graph.as_graph_def().node]
+        x = graph.get_tensor_by_name("t_picture:0")#vrai
+        y = graph.get_tensor_by_name("t_labels:0")#vrai
+        #train = graph.get_operation_by_name("Train_op")
+        #logists = graph.get_operation_by_name("logits")
+        #loss = graph.get_operation_by_name("Loss")
+        train = tf.get_collection('train_op')#vrai
+        loss = tf.get_collection('loss_op')#vrai
+        logists = tf.get_collection('logits_op')#vrai
+        errors = tf.get_collection('errors')#vrai
+        print(errors)
+        #sess.run(tf.global_variables_initializer())
+    else:
+        print("file non exist")
+        # number of classes
+        num_classes = len(classes)
+
+        x = tf.placeholder(tf.float32, [None, height, width, channels],name='t_picture')
+        y = tf.placeholder(tf.float32, [None, num_classes],name='t_labels')
+
+        # sess = tf.InteractiveSession()
+        logits=ResNet50(x, num_classes)
+        # logits = tf.nn.softmax(logits)
+        tf.add_to_collection('logits_op',logits)
+
+        # Define a loss function
+        loss=tf.reduce_mean(tf.abs(y-logits),name='Loss')
+        tf.add_to_collection('loss_op',loss)
+
+        # loss = tf.nn.softmax_cross_entropy_with_logits_v2 (labels=y, logits=logits)
+        # loss=tf.nn.l2_loss(logits - y)
+        # loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(logits), reduction_indices=1))
+
+        # train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+        train = tf.train.MomentumOptimizer(learning_rate=0.1, momentum=0.9).minimize(loss,name='Train_op')
+        tf.add_to_collection('train_op',train)
+
+        # train = tf.train.AdadeltaOptimizer().minimize(loss)
+
+        # correct_prediction=tf.equal(tf.argmax(softmax), tf.argmax(y))
+        # acc=tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        sess.run(tf.global_variables_initializer())
+        errors=[]
+        tf.add_to_collection('errors',errors)
+
+    print("training start")
+
     for _ in range(epochs):
             print("*******************  ", _, "  *******************")
             indice = np.random.permutation(batche_num)
             for i in range(batch_size-1):
                 min_batch = indice[i*batch_size:(i+1)*batch_size]
                 curr_loss, curr_train = sess.run([loss, train], {x: data[min_batch], y: labels[min_batch]})
-                print(_, "-Iteration %d\nloss:\n%s" % (i, curr_loss))
+                print(_, "-Iteration %d loss:\n%s" % (i, curr_loss))
                 errors.append(curr_loss)
     #tf.add_to_collection('errors',errors)
-    saver.save(sess, "Save/dataSaved")
+    saver = tf.train.Saver()
+    saver.save(sess, save_dir+save_file)
     plt.plot(errors, label="loss")
     plt.xlabel('# epochs')
     plt.ylabel('MSE')
